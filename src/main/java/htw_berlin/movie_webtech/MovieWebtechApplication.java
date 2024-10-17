96b528ef7f1aea420f658baad37d49d34c6290a3
marujaangela <s0591654@htw-berlin.de>
2024-10-17 20:02:22 +0200

    First push

---
+++ src/main/java/htw_berlin/movie_webtech/MovieWebtechApplication.java
@@ -0,0 +1,13 @@
+package htw_berlin.movie_webtech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieWebtechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieWebtechApplication.class, args);
+	}
+
+}
---
+++ src/test/java/htw_berlin/movie_webtech/MovieWebtechApplicationTests.java
@@ -0,0 +1,13 @@
+package htw_berlin.movie_webtech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieWebtechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
